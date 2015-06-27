da809082fee8ee686c16c67b92c04762eb5bd2e6
dragonkoko <dragonkoko@abv.bg>
2015-06-27 15:13:16 +0300

    add info for ivo

---
+++ za_ivo.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<?php include('test_code.php') ?>
+</head>
+<body>
+	
+	<form action="" method="POST">
+		<input type="text" name="1" value="Whatever"/><br>
+		<input type="text" name="1_1" value="a"/>
+		<input type="text" name="1_2" value="b"/>
+		<input type="text" name="1_3" value="c"/>
+		<input type="text" name="1_4" value="d"/><br>
+		<input type="text" name="2" value="Whatever"/><br>
+		<input type="text" name="2_1" value="a"/>
+		<input type="text" name="2_2" value="b"/>
+		<input type="text" name="2_3" value="c"/>
+		<input type="text" name="2_4" value="d"/><br>
+		<input type="text" name="3" value="Whatever"/><br>
+		<input type="text" name="3_1" value="a"/>
+		<input type="text" name="3_2" value="b"/>
+		<input type="text" name="3_3" value="c"/>
+		<input type="text" name="3_4" value="d"/><br>
+		<input class= "button" type="submit" name="submit" value="Generate_PDF"/>
+		<input type= "text" name="number_of_questions" value="3"/>
+	</form>
+		
+		
+</body>
+</html>
+
