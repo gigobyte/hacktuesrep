035b811236a79516612760230a882b2d232009a1
gigobyte <gigobest2@gmail.com>
2015-06-27 20:20:10 +0300

    fixed tests

--- test_code.php
+++ test_code.php
@@ -13,37 +13,37 @@ if(isset($_POST['submit'])) {
 
 	for ($i = 1; $i <= $number_of_questions; $i++) {
 		$question = $_POST[$i];
-		$pdf->SetFont("DejaVu","", "20");
+		$pdf->SetFont("DejaVu","", "12");
 		$pdf->write(5, $i);
 		$pdf->write(5, ". ");
 		$pdf->write(5, $question);
 		$pdf->write(5, "\n\n");
 		$answer = $i . "_1";
 		$answer_option = $_POST[$answer];
-		$pdf->SetFont("DejaVu","", "12");
+		$pdf->SetFont("DejaVu","", "10");
 		$pdf->write(5, "A) ");
-		$pdf->SetFont("DejaVu", "", "10");
+		$pdf->SetFont("DejaVu", "", "8");
 		$pdf->write(5, $answer_option);
 		$pdf->write(5, "\n");
 		$answer = $i . "_2";
 		$answer_option = $_POST[$answer];
-		$pdf->SetFont("DejaVu","", "12");
+		$pdf->SetFont("DejaVu","", "10");
 		$pdf->write(5, "B) ");
-		$pdf->SetFont("DejaVu", "", "10");
+		$pdf->SetFont("DejaVu", "", "8");
 		$pdf->write(5, $answer_option);
 		$pdf->write(5, "\n");
 		$answer = $i . "_3";
 		$answer_option = $_POST[$answer];
-		$pdf->SetFont("DejaVu","", "12");
+		$pdf->SetFont("DejaVu","", "10");
 		$pdf->write(5, "C) ");
-		$pdf->SetFont("DejaVu", "", "10");
+		$pdf->SetFont("DejaVu", "", "8");
 		$pdf->write(5, $answer_option);
 		$pdf->write(5, "\n");		
 		$answer = $i . "_4";
 		$answer_option = $_POST[$answer];
-		$pdf->SetFont("DejaVu","", "12");
+		$pdf->SetFont("DejaVu","", "10");
 		$pdf->write(5, "D) ");
-		$pdf->SetFont("DejaVu", "", "10");
+		$pdf->SetFont("DejaVu", "", "8");
 		$pdf->write(5, $answer_option);
 		$pdf->write(5, "\n\n\n");
 	}
