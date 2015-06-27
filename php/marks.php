24803b2388dce2fff4b4fc0716cc2019cc48fdfe
NoHomey <qweqwe123>
2015-06-27 11:51:58 +0300

    refactor

--- php/marks.php
+++ php/marks.php
@@ -1,16 +1,15 @@
 <table>
 	<?php
+		session_start();
 		$query = "SELECT `data`, `predmet`, `ocenka` FROM  `marks`
 		WHERE `name` = `$_SESSION['name']` "; 
-
 		$result = mysql_query($query);
 		while($row = mysql_fetch_row($result)) 
 		{
 			echo "<tr>";
-			for($i = 0; $i < 9;++$i) {
+			for($i = 0; $i < 3;++$i) {
 	    			echo "<td>" . $row[$i] . "</td>"; 
 	    	}
-
 			echo "</tr>";			
 		}
 
