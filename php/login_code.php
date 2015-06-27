29358a59e47d60f44c2640124d556be95d161a55
gigobyte <gigobest2@gmail.com>
2015-06-27 17:19:10 +0300

    fixed chars from sql

--- php/login_code.php
+++ php/login_code.php
@@ -8,6 +8,8 @@ if(isset($_POST['submit'])) {
 	// username and password sent from form 
 	$username=$_POST['username']; 
 	$password=$_POST['password']; 
+	
+	
 
 	// To protect MySQL injection
 	$username = stripslashes($username);
@@ -17,6 +19,7 @@ if(isset($_POST['submit'])) {
 	
 	$select_db = mysql_select_db('users');
 	$query = "SELECT * FROM `members` WHERE username='$username' and password='$password'";
+	
 	$result = mysql_query($query) or die(mysql_error());
 
 	if(mysql_num_rows($result) > 0){
@@ -24,6 +27,7 @@ if(isset($_POST['submit'])) {
 		$_SESSION['password'] = $password;
 		if(isset($_POST['username']) && isset($_POST['password'])){
 			header( "Location: ../index.html" );
+			echo "<p style=\" color:red; font-size: 10px\">$username</p>";
 		}
 		
 	}else{
